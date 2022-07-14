66a696daa4784155546973a16fbb3505dd830d0d
SashaH62 <sashahermanwebdev@gmail.com>
2022-07-14 21:56:43 +0200

    Add transition propert to hands and create separate variables for degrees per hand

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-function time() {
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setTime() {
   const currentTime = new Date();
-  const secondHand = document.querySelector(".second-hand");
-  const minuteHand = document.querySelector(".min-hand");
-  const hourHand = document.querySelector(".hour-hand");
+
   const seconds = currentTime.getSeconds();
+  const secondsDegree = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+
   const minutes = currentTime.getMinutes();
+  const minutesDegree = (minutes / 60) * 360 + 90;
+  minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
+
   const hours = currentTime.getHours();
-  secondHand.style.transform = `rotate(${seconds * 6 + 90}deg)`;
-  minuteHand.style.transform = `rotate(${minutes * 6 + 90}deg)`;
-  hourHand.style.transform = `rotate(${hours * 30 + 90}deg)`;
-  console.log(currentTime);
+  let hoursDegree = undefined;
+  if (hours > 12) {
+    hoursDegree = ((hours - 12) / 12) * 360 + 90;
+  } else {
+    hoursDegree = (hours / 12) * 360 + 90;
+  }
+
+  hourHand.style.transform = `rotate(${hoursDegree}deg)`;
 }
 
-setInterval(time, 1000);
+setInterval(setTime, 1000);
